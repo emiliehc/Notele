2ae486c63a5d64dc2a7a7d30fa8bc5e40f29fea1
Hongjun Chen <hongjun.chen2@mail.mcgill.ca>
2022-03-12 21:45:04 -0500

    equality comparaison

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -21,10 +21,11 @@ public enum Modifier : int
     Flat = -1
 }
 
-public sealed record Tone
+public sealed class Tone : IComparable<Tone>
 {
     public ToneLetter toneLetter { get; private set; } = default;
     public Modifier modifier { get; private set; } = None;
+    public int relativeValue => (int) this;
 
     public static implicit operator (ToneLetter, Modifier)(Tone record)
     {
@@ -58,24 +59,33 @@ public sealed record Tone
         _toneLetter = toneLetter;
         _modifier = modifier;
     }
-    
-    
+
+    public int CompareTo(Tone other) => relativeValue.CompareTo(other.relativeValue);
+
+    public override bool Equals(object obj) => this switch
+    {
+        { } other => relativeValue == other.relativeValue
+    };
+
+    public static bool operator ==(Tone lhs, Tone rhs)
+        => lhs != null && lhs.Equals(rhs);
+
+    public static bool operator !=(Tone lhs, Tone rhs) => !(lhs == rhs);
+
+    public override int GetHashCode() => relativeValue.GetHashCode();
 }
 
-public record Note : IComparable<Note>
+public sealed class Note : IComparable<Note>
 {
     public Tone tone { get; private set; } = default;
     public int octave { get; private set; } = default;
+    public int relativeValue => (int) this;
 
     public static implicit operator (Tone, int)(Note record)
-    {
-        return (record.tone, record.octave);
-    }
+        => (record.tone, record.octave);
 
     public static implicit operator Note((Tone, int) tuple)
-    {
-        return new Note {tone = tuple.Item1, octave = tuple.Item2};
-    }
+        => new() {tone = tuple.Item1, octave = tuple.Item2};
 
     public override string ToString() => tone.ToString() + octave;
 
@@ -99,5 +109,21 @@ public record Note : IComparable<Note>
     public static readonly Note A4 = (A, 4);
     public static readonly float stepFactor = Mathf.Pow(2, 1.0f / 12.0f);
 
-    public int CompareTo(Note other) => ((int) this).CompareTo((int) other);
+    public int CompareTo(Note other) => relativeValue.CompareTo(other.relativeValue);
+
+    public override bool Equals(object obj) => this switch
+    {
+        { } other => relativeValue == other.relativeValue
+    };
+
+    public static bool operator ==(Note lhs, Note rhs)
+        => lhs != null && lhs.Equals(rhs);
+
+    public static bool operator !=(Note lhs, Note rhs) => !(lhs == rhs);
+
+    public override int GetHashCode() => relativeValue.GetHashCode();
+}
+
+public static class Program
+{
 }
