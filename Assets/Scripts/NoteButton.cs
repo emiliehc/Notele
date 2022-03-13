774cafb6bf20373e5b52bb1e981ef4e08bf809a7
Ezra Marin <marinezra02@gmail.com>
2022-03-13 10:54:11 -0400

    latest changes from last night

--- Assets/Scripts/NoteButton.cs
+++ Assets/Scripts/NoteButton.cs
@@ -14,21 +14,26 @@ public class NoteButton : MonoBehaviour
     public Modifier modifier;
     public int octave;
     public bool isEnabled = false;
+    public static List<Note> NoteList = List.Of<Note>();
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // void Start()
+    // {
+    //     
+    // }
 
     public void Toggle()
     {
         if (isEnabled)
         {
             transform.localScale += new Vector3(0.05f, 0.05f);
+            NoteList = List.Remove(NoteList, note);
+            NoteList = List.Sort(NoteList, (note1, note2) => note1.CompareTo(note2));
         }
         else
         {
             transform.localScale -= new Vector3(0.05f, 0.05f);
+            NoteList = NoteList.Append(note);
+            NoteList = List.Sort(NoteList, (note1, note2) => note1.CompareTo(note2));
         }
 
         isEnabled = !isEnabled;
@@ -40,8 +45,8 @@ public class NoteButton : MonoBehaviour
     
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // void Update()
+    // {
+    //     print(List.StringOf(NoteList));
+    // }
 }
